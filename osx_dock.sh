572802294a75d95650f8bb911cc02f9d3bba0438
Celso Fernandes <celso.fernandes@gmail.com>
2017-08-27 00:36:28 -0300

    Check for dockutils and fix apps names on osx_dock.sh

--- osx_dock.sh
+++ osx_dock.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
+which -s dockutil > /dev/null
+
+if [ $? -ne 0 ]; then
+  echo "dockutil not found, please install it from \`assets\` path"
+  exit 1
+fi
 
 # can we do this using boxen?
 defaults write com.apple.dock static-only -bool TRUE
 defaults write com.apple.dock tilesize -int 40
 
 # Remove Default Apps
-for app in Siri Launchpad Safari Mail Contacts Calendar Notes Reminders Maps Photos Messages FaceTime Pages Numbers Keynote iTunes iBooks "App Store" "System Preferences"; do dockutil --remove $app --allhomes --no-restart; done
+for app in Siri Launchpad Safari Mail Contacts Calendar Notes Reminders Maps Photos Messages FaceTime Pages Numbers Keynote iTunes iBooks \"App Store\" \"System Preferences\"; do dockutil --remove $app --allhomes --no-restart; done
 
 # Add Chrome, Terminal and Atom as static items
 dockutil --add /Applications/Google\ Chrome.app --allhomes --no-restart
